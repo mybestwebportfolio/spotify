fc67ebb31520d1e81d9fde8bdb03601968db55e1
KhalifRafi <71247773+KhalifRafi@users.noreply.github.com>
2023-10-18 10:59:21 +0700

    second commit

--- components/PlayButton.tsx
+++ components/PlayButton.tsx
@@ -6,13 +6,11 @@ import { Song } from "@/types"
 interface PlayButtonProps {
   className?: string
   size?: number
-  onClick: (id: string) => void
-  data: Song
 }
 
-const PlayButton = ({className, size, onClick, data}: PlayButtonProps) => {
+const PlayButton = ({className, size}: PlayButtonProps) => {
   return (
-    <button onClick={() => onClick(data.id)} className={twMerge(`bg-green-600 rounded-full flex items-center p-3 opacity-0 group-hover:opacity-100 hover:scale-105 transition`, className)}>
+    <button className={twMerge(`bg-green-600 rounded-full flex items-center p-3 opacity-0 group-hover:opacity-100 hover:scale-105 transition`, className)}>
         <FaPlay className="text-black pl-1" size={size}/>
     </button>
   )
